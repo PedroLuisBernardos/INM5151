b4266fb3b6bd849be8884ca0db865589d2d6eb13
Willy Tim <tim.willy@courrier.uqam.ca>
2021-08-12 02:18:17 -0400

    Ajout de fonctionnalité changement de theme sombre/clair

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,1 +1,35 @@
 /* JS général de l'application */
+
+let theme = localStorage.getItem('theme');
+const changeThemeToDark = () => {
+    document.documentElement.setAttribute("theme", "dark") 
+    localStorage.setItem("theme", "dark") 
+}
+
+const changeThemeToLight = () => {
+    document.documentElement.setAttribute("theme", "light") 
+    localStorage.setItem("theme", 'light') 
+}
+
+if(theme) {
+    let test = localStorage.getItem('theme');
+    if(test === 'dark') {
+        changeThemeToDark()
+    } else {
+        changeThemeToLight()
+    }
+}
+
+// Chercher l'élément permettant le switch de thème
+const checkbox = document.getElementById("switch");
+
+// Appliquer le changement
+checkbox.addEventListener('click', () => {
+    let theme = localStorage.getItem('theme'); 
+    if (theme ==='dark'){
+        changeThemeToLight()
+    }else{
+        changeThemeToDark()
+    }   
+});
+
